196f7d48599deb8886354f1e4faf09c42c348982
alienthebetrayer <gpichkurov@gmail.com>
2026-04-01 16:50:45 +0300

    +<Button/>, +Ripple effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-    return (
-        <div className="flex flex-col gap-2">
-            <span>init</span>
-        </div>
-    );
+    return null;
 }
--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,3 +1,29 @@
-export const Button = () => {
-    return null;
+"use client";
+import { rippleEnable } from "@/src/shared/lib/ripple";
+import { ComponentPropsWithoutRef } from "react";
+
+type Props = {
+    isEnabled?: boolean;
+} & ComponentPropsWithoutRef<"button">;
+
+export const Button = ({ isEnabled = true, onPointerDown, onPointerEnter, className, children, ...rest }: Props) => {
+    return (
+        <button
+            disabled={!isEnabled}
+            className={`button ripple ${className ?? ""} ${isEnabled ? "" : "opacity-30 pointer-events-none"}`}
+            onPointerDown={(e) => {
+                rippleEnable(e);
+                onPointerDown?.(e);
+            }}
+            onPointerEnter={(e) => {
+                if (e.buttons & 1) {
+                    rippleEnable(e);
+                }
+                onPointerEnter?.(e);
+            }}
+            {...rest}
+        >
+            {children}
+        </button>
+    );
 };
